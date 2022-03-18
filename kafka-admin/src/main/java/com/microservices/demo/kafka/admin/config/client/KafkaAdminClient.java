4452c0f0df27afc9e6f0aa33c4d07a2030428f5c
Diego Fialho Rodrigues <diego.fialho@ufv.br>
2022-03-18 17:17:18 -0300

    3-14

---
+++ kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
@@ -0,0 +1,11 @@
+package com.microservices.demo.kafka.admin.config.client;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class KafkaAdminClient {
+    
+}
