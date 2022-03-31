ccb2eadfbc687142fd800f0ab7001abdba73816a
Diego Fialho Rodrigues <diego.fialho@ufv.br>
2022-03-31 13:59:40 -0300

    3-17

--- app-config-data/src/main/java/com/microservices/demo/config/KafkaProducerConfigData.java
+++ app-config-data/src/main/java/com/microservices/demo/config/KafkaProducerConfigData.java
@@ -1,5 +1,8 @@
 package com.microservices.demo.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 import lombok.Data;
 
 @Data
