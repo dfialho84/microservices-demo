e3869dddc5603346d4fa4da703e76fd9f1c0a02e
Diego Fialho Rodrigues <diego.fialho@ufv.br>
2022-04-13 11:02:32 -0300

    4-27

--- config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
+++ config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
@@ -3,6 +3,7 @@ package com.microservices.demo.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableConfigServer
 @SpringBootApplication
